ee6f4e3ed0aae07b0b6a32153c0aeefc5c2d7289
Oussama Lmekkeddem <lmousolm@gmail.com>
2024-12-10 18:13:25 +0100

    first update

--- extra-addons/app_pharmacy/controllers/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import PharmacyController
-from . import HomeController
--- extra-addons/app_pharmacy/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from . import Pharmacy
-from . import Medicines
-from . import Pharmacy_Permanence
-from . import stock
-from . import commande
